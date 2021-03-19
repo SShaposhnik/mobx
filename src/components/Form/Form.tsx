560d97dd343953fc818af73cb347aaa6af7e9a84
sshaposhnik <sshaposhnik.sergey88@gmail.com>
2021-03-19 16:33:04 +0700

    init project

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IFormProps } from 'interfaces'
+
+
+const Form: React.FC<IFormProps> = (props) => {
+
+  return (
+    <div>
+      123
+    </div>
+  )
+}
+
+export default Form
